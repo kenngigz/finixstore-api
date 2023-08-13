01bf9d88e267cf66bf35bb23e8d23d0bb6593fd0
Kenneth Ngige <malkiellov@gmail.com>
2023-08-13 22:58:02 +0300

    Updated code to fix issue #123

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "node_mysql_sequelize_db",
-    dialect: "mysql",
-  
-    pool: {
-      max: 1,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  };
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
+  dialect: process.env.dialect,
+
+  pool: {
+    max: 1,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
